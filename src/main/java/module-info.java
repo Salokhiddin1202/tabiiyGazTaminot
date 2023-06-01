e32fea04f2c99303c1a6c17b4b5fbaf0fab33897
salohiddin <salohiddinurinboev1203@gmail.com>
2023-06-02 03:04:00 +0500

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.tabiiygaztaminot {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.tabiiygaztaminot to javafx.fxml;
+    exports com.example.tabiiygaztaminot;
+}
